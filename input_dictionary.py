801fe2368cfb61b64baa23a5aa8fb4f1cb7ee686
Carl-dc09 <diamantecarl9@gmail.com>
2024-10-30 22:36:11 +0800

    store to dictionary every input

--- input_dictionary.py
+++ input_dictionary.py
@@ -1,6 +1,6 @@
 # Create a dictionary to store the information
 def input_dictionary():
-    ranges = {
+    input_num_ranges = {
         "1 - 10": [],
         "11 - 20": [],
         "21 - 30": [],
@@ -14,4 +14,16 @@ def input_dictionary():
             input_number = int(input("Enter a number (between 1 and 50): "))
             if input_number < 1 or input_number > 50:
                 break
+
+            # Store the number that's been inputted in the dictionary
+            elif 1 <= input_number <= 10:
+                input_num_ranges["1 - 10"].append(input_number)
+            elif 11 <= input_number <= 20:
+                input_num_ranges["11 - 20"].append(input_number)
+            elif 21 <= input_number <= 30:
+                input_num_ranges["21 - 30"].append(input_number)
+            elif 31 <= input_number <= 40:
+                input_num_ranges["31 - 40"].append(input_number)
+            elif 41 <= input_number <= 50:
+                input_num_ranges["41 - 50"].append(input_number)
 # Print
