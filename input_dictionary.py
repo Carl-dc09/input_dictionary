b00a4f024a5cab1e941b0287b94014c4b7081c9a
Carl-dc09 <diamantecarl9@gmail.com>
2024-10-30 22:16:19 +0800

    a working dictionary

--- input_dictionary.py
+++ input_dictionary.py
@@ -1,3 +1,11 @@
 # Create a dictionary to store the information
+def input_dictionary():
+    ranges = {
+        "1 - 10": [],
+        "11 - 20": [],
+        "21 - 30": [],
+        "31 - 40": [],
+        "41 - 50": []
+    }
 # Create an input
 # Print
